92248a5d990442d25cd7f919b5b3ff1378165e82
jeff-mcc <reffery@gmail.com>
2021-06-03 18:32:46 -0600

    worked on adding a responsive menu to the home screen

--- biomechweb/src/components/Contact.js
+++ biomechweb/src/components/Contact.js
@@ -8,8 +8,9 @@ const Contact = () => {
                 <Header />
                 <h2 className="banner">Contact</h2>
                 <div className="centerBox">
-                    <p>Email: email@email.email</p>
-                    <p>Location: 1234 Anywhere St, Nowhere, NO 56789</p>
+                    <p className="contactInfo">Email: email@email.email</p>
+                    <p className="contactInfo">Phone: 555-555-5555</p>
+                    <p className="contactInfo">Location: 1234 Anywhere St, Nowhere, NO 56789</p>
                 </div>
             </div>
             <Footer />
--- biomechweb/src/components/Header.js
+++ biomechweb/src/components/Header.js
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom'
 const Header = () => {
     return(
         <header className="userHead">
-            <Link className="links" to="/"><h2>BiomechWeb</h2></Link>
-            <div>
+            <Link className="links homelink" to="/"><h2>BiomechWeb</h2></Link>
+            <div className="menu">
                 {/* <Link to="/">Home</Link> */}
-                <Link className="links" to="/data">View Data</Link>
-                <Link className="links" to="/about">About</Link>
-                <Link className="links" to="/contact">Contact</Link>
-                <Link className="links" to="/login">Login/Signup</Link>
+                <Link className="links menulinks" to="/data">View Data</Link>
+                <Link className="links menulinks" to="/about">About</Link>
+                <Link className="links menulinks" to="/contact">Contact</Link>
+                <Link className="links menulinks" to="/login">Login/Signup</Link>
             </div>
         </header>
     )
