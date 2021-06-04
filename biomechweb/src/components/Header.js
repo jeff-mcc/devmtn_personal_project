06bc7d237cf717f472c0ad3dbb6ec7460feb3a26
jeff-mcc <reffery@gmail.com>
2021-06-04 12:57:32 -0600

    added animation to home menu

--- biomechweb/src/components/Header.js
+++ biomechweb/src/components/Header.js
@@ -6,18 +6,19 @@ const Header = () => {
 
     const handleBool = () => {
         setMenuBool(!menuBool)
-        console.log(menuBool)
+        // console.log(menuBool)
     }
 
     return(
         <header className="userHead">
             <Link className="links homelink" to="/"><h2>BiomechWeb</h2></Link>
-            <div className="menu">
-                <button className="openClose" onClick={handleBool}>X</button>
-                <Link className="links menulinks" to="/data">View Data</Link>
-                <Link className="links menulinks" to="/about">About</Link>
-                <Link className="links menulinks" to="/contact">Contact</Link>
-                <Link className="links menulinks" to="/login">Login/Signup</Link>
+            <div className={menuBool ? "menu openMenu" : "menu"}>
+                <button className={menuBool ? "openClose open" : "openClose"} onClick={handleBool}>X</button>
+                <div className={menuBool ? "showBurger closeBurger" : "showBurger"}>_<p className="break"></p>_<p className="break"></p>_</div>
+                <Link className={menuBool ? "links menulinks openMenuLinks" : "links menulinks"} to="/data">View Data</Link>
+                <Link className={menuBool ? "links menulinks openMenuLinks" : "links menulinks"} to="/about">About</Link>
+                <Link className={menuBool ? "links menulinks openMenuLinks" : "links menulinks"} to="/contact">Contact</Link>
+                <Link className={menuBool ? "links menulinks openMenuLinks" : "links menulinks"} to="/login">Login/Signup</Link>
             </div>
         </header>
     )
