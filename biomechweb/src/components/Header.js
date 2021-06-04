70f85bd240633d7ac05b22b5f92255478f9a7293
jeff-mcc <reffery@gmail.com>
2021-06-03 23:15:38 -0600

    added placeholder images and worked on setting up menu to eventually animate open and close

--- biomechweb/src/components/Header.js
+++ biomechweb/src/components/Header.js
@@ -1,11 +1,19 @@
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 const Header = () => {
+    const [menuBool,setMenuBool] = useState(false)
+
+    const handleBool = () => {
+        setMenuBool(!menuBool)
+        console.log(menuBool)
+    }
+
     return(
         <header className="userHead">
             <Link className="links homelink" to="/"><h2>BiomechWeb</h2></Link>
             <div className="menu">
-                {/* <Link to="/">Home</Link> */}
+                <button className="openClose" onClick={handleBool}>X</button>
                 <Link className="links menulinks" to="/data">View Data</Link>
                 <Link className="links menulinks" to="/about">About</Link>
                 <Link className="links menulinks" to="/contact">Contact</Link>
--- biomechweb/src/components/Home.js
+++ biomechweb/src/components/Home.js
@@ -6,9 +6,11 @@ const Home = () => {
         <div className="handleCenter">
             <div className="content">
                 <Header />
-                <br></br>
-                <p>text to fill up space in the middle of the page</p>
-                <br></br>
+                <div className="imgContainer">
+                    <img className="homeImg" src="https://via.placeholder.com/1000/B58981/9B2E45/?text=View+Data+On+BiomechWeb" alt="placeholder"></img>
+                    <img className="homeImg" src="https://via.placeholder.com/1000/9B2E45/F5DCD5/?text=Sign+Up+For+An+Account" alt="placeholder"></img>
+                    <img className="homeImg" src="https://via.placeholder.com/1000/8C544C/9B2E45/?text=Contribute+Data" alt="placeholder"></img>
+                </div>
             </div>
             <Footer />
         </div>
