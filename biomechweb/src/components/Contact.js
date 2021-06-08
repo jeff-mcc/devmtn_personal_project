b14d78fa0945d39583647c7a85879b778748528f
jeff-mcc <reffery@gmail.com>
2021-06-08 15:03:30 -0600

    updated personal info on about page

--- biomechweb/src/components/Contact.js
+++ biomechweb/src/components/Contact.js
@@ -8,9 +8,9 @@ const Contact = () => {
                 <Header />
                 <h2 className="banner">Contact</h2>
                 <div className="centerBox">
-                    <p className="contactInfo">Email: email@email.email</p>
-                    <p className="contactInfo">Phone: 555-555-5555</p>
-                    <p className="contactInfo">Location: 1234 Anywhere St, Nowhere, NO 56789</p>
+                    <p className="contactInfo">Email: reffery@gmail.com</p>
+                    <p className="contactInfo contactLink">LinkedIn: &nbsp; <a className="contactLinkedIn" href="https://www.linkedin.com/in/jeffrey-mcclellan-phd/"><p className="marginSet">linkedin.com/in/jeffrey-mcclellan-phd/</p></a></p>
+                    <p className="contactInfo">Location: Alpine, UT 84004</p>
                 </div>
             </div>
             <Footer />
@@ -19,3 +19,6 @@ const Contact = () => {
 }
 
 export default Contact
+
+// <p className="contactInfo">Phone: 555-555-5555</p>
+// <p className="contactInfo">Location: 1234 Anywhere St, Nowhere, NO 56789</p>
